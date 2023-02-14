0195ccc62b633a1535d5e5d039f9b9ef66736088
Alessandro-Villani <alessvill@libero.it>
2023-02-14 16:04:20 +0100

    es. 1 group by

--- tracce SELECT & GROUP BY.sql
+++ tracce SELECT & GROUP BY.sql
@@ -52,6 +52,9 @@ WHERE `phone` IS NULL;
 
 -- 1. Contare quanti iscritti ci sono stati ogni anno
 
+SELECT COUNT(*) AS 'n_new_students', YEAR(`enrolment_date`) AS 'enrolment_year'
+FROM `students`
+GROUP BY YEAR(`enrolment_date`);
 
 -- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
 
