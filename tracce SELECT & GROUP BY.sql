0bfe6902beb1a9444b20493e0a2c84afab54632b
Alessandro-Villani <alessvill@libero.it>
2023-02-14 15:47:41 +0100

    es. 4

--- tracce SELECT & GROUP BY.sql
+++ tracce SELECT & GROUP BY.sql
@@ -20,6 +20,9 @@ WHERE DATEDIFF(CURDATE(), `date_of_birth`) / 365 >= 30;
 
 -- 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
 
+SELECT `name`, `period`, `year`
+FROM `courses`
+WHERE `period` = 'I semestre' AND `year` = 1;
 
 -- 5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21)
 
