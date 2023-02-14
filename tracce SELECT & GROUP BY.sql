915fef2e1ced080ae14a58e58d9185c3856a994e
Alessandro-Villani <alessvill@libero.it>
2023-02-14 15:29:41 +0100

    es. 2

--- tracce SELECT & GROUP BY.sql
+++ tracce SELECT & GROUP BY.sql
@@ -2,12 +2,15 @@
 
 -- 1. Selezionare tutti gli studenti nati nel 1990 (160)
 
-SELECT `name`, `surname`, YEAR(`date_of_birth`) AS `year`
+SELECT `name`, `surname`, YEAR(`date_of_birth`) AS 'year'
 FROM `students`
 WHERE YEAR(`date_of_birth`) = 1990;
 
 -- 2. Selezionare tutti i corsi che valgono più di 10 crediti (479)
 
+SELECT `name` AS 'course_name',  `cfu`
+FROM `courses`
+WHERE `cfu` > 10;
 
 -- 3. Selezionare tutti gli studenti che hanno più di 30 anni
 
