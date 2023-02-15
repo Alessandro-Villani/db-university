727d968490cb14e338f08ca9807129af6dddb1b1
Alessandro-Villani <alessvill@libero.it>
2023-02-15 15:47:07 +0100

    es. 5 Join

--- tracce JOIN.sql
+++ tracce JOIN.sql
@@ -37,7 +37,15 @@ ORDER BY `students`.`surname`, `students`.`name`;
 
 -- 5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
 
-
+SELECT `degrees`.`name` AS 'degree_name', `courses`.`name` AS 'course_name', `teachers`.`name` AS 'teacher_name', `teachers`.`surname` AS 'teacher_surname'
+FROM `teachers`
+JOIN `course_teacher`
+ON `teachers`.`id` = `course_teacher`.`teacher_id`
+JOIN `courses`
+ON `courses`.`id` = `course_teacher`.`course_id`
+JOIN `degrees`
+ON `degrees`.`id` = `courses`.`degree_id`
+ORDER BY `degrees`.`name`;
 
 -- 6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
 
